b3d9c4e33edd36afd3eddd55d84f7c14d3860a00
Bichitra Gautam <bichitragautam25@gmail.com>
2024-08-20 20:50:35 +0545

    chore: Refactor Navbar component and update navigation links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,76 +26,73 @@ const Navbar = () => {
   }, []);
 
   return (
-      <nav
-          className={`${
-              styles.paddingX
-          } w-full flex items-center py-5 fixed top-0 z-20 ${
-              scrolled ? "bg-primary" : "bg-transparent"
-          }`}
-      >
-        <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
-          <Link
-              to='/'
-              className='flex items-center gap-2'
-              onClick={() => {
-                setActive("");
-                window.scrollTo(0, 0);
-              }}
-          >
-            <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
-            <p className='text-white text-[18px] font-bold cursor-pointer flex '>
-              Bichitra&nbsp;Gautam&nbsp;
-              <span className='sm:block hidden'>|&nbsp;Simple Logic Doesn't Work</span>
-            </p>
-          </Link>
+    <nav
+      className={`${styles.paddingX
+        } w-full flex items-center py-5 fixed top-0 z-20 ${scrolled ? "bg-primary" : "bg-transparent"
+        }`}
+    >
+      <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
+        <Link
+          to='/'
+          className='flex items-center gap-2'
+          onClick={() => {
+            setActive("");
+            window.scrollTo(0, 0);
+          }}
+        >
+          <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
+          <p className='text-white text-[18px] font-bold cursor-pointer flex '>
+            Bichitra&nbsp;Gautam&nbsp;
+            <span className='sm:block hidden'>|&nbsp;Simple Logic Doesn't Work</span>
+          </p>
+        </Link>
 
-          <ul className='list-none hidden sm:flex flex-row gap-10'>
-            {navLinks.map((nav) => (
+        <ul className='list-none hidden sm:flex flex-row gap-10'>
+          {navLinks.map((nav) => (
+            <li
+              key={nav.id}
+              className={`${active === nav.title ? "text-white" : "text-secondary"
+                } hover:text-white text-[18px] font-medium cursor-pointer`}
+              onClick={() => setActive(nav.title)}
+            >
+              <a href={nav.url ? nav.url : `#${nav.id}`} target={nav.url ? "_blank" : "_self"} rel={nav.url ? "noopener noreferrer" : ""}>
+                {nav.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+
+        <div className='sm:hidden flex flex-1 justify-end items-center'>
+          <img
+            src={toggle ? close : menu}
+            alt='menu'
+            className='w-[28px] h-[28px] object-contain'
+            onClick={() => setToggle(!toggle)}
+          />
+
+          <div
+            className={`${!toggle ? "hidden" : "flex"
+              } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
+          >
+            <ul className='list-none flex justify-end items-start flex-1 flex-col gap-4'>
+              {navLinks.map((nav) => (
                 <li
-                    key={nav.id}
-                    className={`${
-                        active === nav.title ? "text-white" : "text-secondary"
-                    } hover:text-white text-[18px] font-medium cursor-pointer`}
-                    onClick={() => setActive(nav.title)}
+                  key={nav.id}
+                  className={`font-poppins font-medium cursor-pointer text-[16px] ${active === nav.title ? "text-white" : "text-secondary"
+                    }`}
+                  onClick={() => {
+                    setToggle(!toggle);
+                    setActive(nav.title);
+                  }}
                 >
                   <a href={`#${nav.id}`}>{nav.title}</a>
                 </li>
-            ))}
-          </ul>
-
-          <div className='sm:hidden flex flex-1 justify-end items-center'>
-            <img
-                src={toggle ? close : menu}
-                alt='menu'
-                className='w-[28px] h-[28px] object-contain'
-                onClick={() => setToggle(!toggle)}
-            />
-
-            <div
-                className={`${
-                    !toggle ? "hidden" : "flex"
-                } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
-            >
-              <ul className='list-none flex justify-end items-start flex-1 flex-col gap-4'>
-                {navLinks.map((nav) => (
-                    <li
-                        key={nav.id}
-                        className={`font-poppins font-medium cursor-pointer text-[16px] ${
-                            active === nav.title ? "text-white" : "text-secondary"
-                        }`}
-                        onClick={() => {
-                          setToggle(!toggle);
-                          setActive(nav.title);
-                        }}
-                    >
-                      <a href={`#${nav.id}`}>{nav.title}</a>
-                    </li>
-                ))}
-              </ul>
-            </div>
+              ))}
+            </ul>
           </div>
         </div>
-      </nav>
+      </div>
+    </nav>
   );
 };
 
