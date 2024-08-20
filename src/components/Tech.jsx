36190734c4830216ed54307ecf2006e5215b64aa
Bichitra Gautam <bichitragautam25@gmail.com>
2024-08-20 21:33:59 +0545

    Update package.json version to 1.0.0 and refactor Tech.jsx and SectionWrapper.jsx components

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,19 +1,16 @@
-import React from "react";
-
+import React, { memo } from "react";
 import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
 
-const Tech = () => {
-  return (
-      <div className='flex flex-row flex-wrap justify-center gap-10'>
-        {technologies.map((technology) => (
-            <div className='w-28 h-28' key={technology.name}>
-              <BallCanvas icon={technology.icon} />
-            </div>
-        ))}
+const Tech = () => (
+  <div className='flex flex-row flex-wrap justify-center gap-10'>
+    {technologies.map((technology) => (
+      <div className='w-28 h-28' key={technology.name}>
+        <BallCanvas icon={technology.icon} />
       </div>
-  );
-};
+    ))}
+  </div>
+);
 
-export default SectionWrapper(Tech, "");
+export default SectionWrapper(memo(Tech), "");
